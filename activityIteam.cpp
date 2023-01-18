9c49ea00e7146b9826e76766bb0241b7219083aa
MD.Emran Alam <105363909+emranCoder@users.noreply.github.com>
2023-01-18 20:14:41 +0600

    Activity selection code

---
+++ activityIteam.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+struct acitivity
+{
+    char c;
+    int first_time, second_time;
+};
+
+bool aSort(acitivity a1, acitivity a2)
+{
+    return (a1.second_time < a2.second_time);
+}
+
+
+int main()
+{
+    int n;
+    cout << "Enter Activity Number: ";
+    cin >> n;
+    acitivity aArr[n];
+    for(int i = 0; i<n; i++)
+    {
+        cin >> aArr[i].c >> aArr[i].first_time >> aArr[i].second_time;
+    }
+    sort(aArr,aArr+n, aSort);
+    cout << "Activity Numbers sort by end time :" << endl;
+    for(int i = 0; i<n; i++)
+    {
+        cout << aArr[i].c << " " <<aArr[i].first_time << " " <<  aArr[i].second_time << endl;
+    }
+    cout << " " << "Activity Number : " << endl;
+    int i = 0;
+    cout<< aArr[i].c << " " <<aArr[i].first_time << " " <<  aArr[i].second_time << endl;
+
+    for(int j = 1; j<n; j++)
+    {
+        if(aArr[j].first_time>=aArr[i].second_time)
+        {
+            cout << aArr[j].c << " " <<aArr[j].first_time << " " <<  aArr[j].second_time << endl;
+            i=j;
+        }
+    }
+
+    return 0;
+}
